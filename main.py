0e97ca9035214c3ec64cca165156ebf04bba9978
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2023-12-18 10:40:40 -0700

    Changes

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import requests
 import time
 
 KEY = "6583339131:AAEH2sySsJxdgQCBBjRJdfwPAMvY9sq9-Sg"
+time = 0
+
+def send_message(text: str) -> None:
+    try:
+        bot = telebot.TeleBot(KEY)
+        bot.send_message(chat_id="-4096042993", text = text)
+    except Exception as e:
+        print("no can send msg. :(") 
+        print(e.with_traceback())
+    
 
 while True:
     resp = requests.get("https://www.boisestate.edu/housing-apartments/apartments-availability/")
@@ -12,10 +22,10 @@ while True:
         print(f"{time.time()} No apts")
     else:
         print(f"{time.time()} Apts!")
-    try:
-        bot = telebot.TeleBot(KEY)
-        bot.send_message(chat_id="@boise_housing_monitor", text = "Helo")
-    except Exception as e:
-        print("no can send msg. :(") 
-        print(e.with_traceback())
+        send_message("Apartments listed!") 
+         
+    time += 10
+    if time % 600 == 0 :
+        send_message("Scanned for 10 mins, no updates.")
+        
     time.sleep(10)
